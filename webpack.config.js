34fd9937b36d11d4effb77996c4c5cb645b2ae33
Emmanuel Phirmis <meagamanu@gmail.com>
2020-01-30 15:05:42 +0100

    add dev-server & clean webpacks

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   module: {
@@ -25,6 +26,11 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "src/index.html"
-    })
-  ]
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  devServer: {
+    contentBase: './dist',
+    open: true
+  }
 };
